663296b140e1f090baceaa1cf0ddb42bf3e930ba
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-13 11:26:31 -0800

    Fix projDetails router for export.

--- router/projectDetailsRouter.js
+++ router/projectDetailsRouter.js
@@ -36,7 +36,7 @@ router.get('/:id', validateId, (req, res) => {
 router.post('/', validatePost, (req, res) => {
     const projBody = req.body
 
-    prjDetDb.insert(projBody)
+    prjDetDb.add(projBody)
         .then(createProj => {
             res.status(200).json(createProj)
         })
@@ -113,3 +113,5 @@ function validatePost(req, res, next) {
         next();
     }
 }
+
+module.exports = router;
