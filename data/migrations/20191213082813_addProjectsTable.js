f071bbe3534993ea4431c5e73e413dab970a0f3f
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-13 08:46:46 -0800

    Create task table.

--- data/migrations/20191213082813_addProjectsTable.js
+++ data/migrations/20191213082813_addProjectsTable.js
@@ -1,13 +1,25 @@
-
 exports.up = function(knex) {
-  return knex.schema.createTable('projects', tbl => {
-      tbl.increments();
-      tbl.string('name', 128).notNullable();
-      tbl.string('description', 255);
-      tbl.boolean('completed').notNullable();
-  })
+	return knex.schema
+		.createTable('projects', tbl => {
+			tbl.increments();
+			tbl.string('name', 128).notNullable();
+			tbl.string('description', 255);
+			tbl.boolean('completed').notNullable();
+		})
+		.createTable('task', tbl => {
+			tbl.increments();
+			tbl.string('description', 512).notNullable();
+			tbl.string('notes', 512);
+			tbl.boolean('completed').notNullable();
+			tbl
+				.integer('project_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('projects')
+				.onDelete('CASCADE') // CASCADE, RESTRICT, DO NOTHING, SET NULL,
+				.onUpdate('CASCADE');
+		});
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function(knex) {};
