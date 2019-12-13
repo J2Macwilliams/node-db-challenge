ca2f1091ed1dafdc891d222edf4324f539d80885
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-13 08:38:12 -0800

    Create projects table.

--- data/migrations/20191213082813_addProjectsTable.js
+++ data/migrations/20191213082813_addProjectsTable.js
@@ -1,6 +1,11 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('projects', tbl => {
+      tbl.increments();
+      tbl.string('name', 128).notNullable();
+      tbl.string('description', 255);
+      tbl.boolean('completed').notNullable();
+  })
 };
 
 exports.down = function(knex) {
